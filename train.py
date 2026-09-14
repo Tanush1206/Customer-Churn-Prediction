6f0a5e07a6955dcf97663d09426116d4ea1728d8
Tanush Thakran <tanushthakran@gmail.com>
2026-09-14 16:08:32 +0530

    Train logistic regression baseline

--- train.py
+++ train.py
@@ -4,6 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, classification_report
+
 
 df = pd.read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
@@ -286,3 +289,38 @@ preprocessor = ColumnTransformer(
         )
     ]
 )
+
+
+# =======================
+# 12. Logistic Regression
+# =======================
+
+logistic_model = Pipeline(
+    steps=[
+        ("preproceessor", preprocessor),
+        ("classsifer", LogisticRegression(max_iter=1000))
+    ]
+)
+
+# Train the model
+logistic_model.fit(X_train, y_train)
+
+# Predictions
+y_pred = logistic_model.predict(X_test)
+
+#Probability of churn = Yes
+y_prob = logistic_model.predict_proba(X_test)[:,1]
+
+# ====================
+# 13. Model Evaluation
+# ====================
+
+print("\nModel Evaluation:")
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1 Score:", f1_score(y_test, y_pred))
+print("ROC-AUC:", roc_auc_score(y_test, y_prob))
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
